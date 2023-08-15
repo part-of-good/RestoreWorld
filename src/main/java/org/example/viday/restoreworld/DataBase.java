27210472571edefb7f4b9f0bc032eb395a889a3f
VidayRU <aptem.lebeshkov@gmail.com>
2023-08-15 22:31:44 +0700

    IT'S WORK!!!!!

--- src/main/java/org/example/viday/restoreworld/DataBase.java
+++ src/main/java/org/example/viday/restoreworld/DataBase.java
@@ -17,148 +17,107 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 public class DataBase {
     public static HikariDataSource hds;
     public Connection con;
     private long count = 102161;
-    private boolean isFinished = false;
     private @NotNull BukkitTask async;
     private String timePrefix;
     private String precent;
-    private HashMap<Location, Long> checkTime = new HashMap<>();
+    private Store store;
+    private final HashMap<BlockData, Long> checkTime = new HashMap<>();
+    private int sizeToFinish = 0;
+    private boolean isStartedSetBlock = false;
+    private boolean isFinishedQuery = false;
 
-    public DataBase(HikariConfig config) {
+
+    public DataBase(HikariConfig config, Store store) {
         hds = new HikariDataSource(config);
+        this.store = store;
         try {
             con = hds.getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        startAsyncQuery();
         async = Bukkit.getScheduler().runTaskTimer(RestoreWorld.getInstance(), this::setBlock, 0, 1);
     }
 
-    public void updateBlocksTest(){
-        new BukkitRunnable(){
+    private void startAsyncQuery() {
+        new BukkitRunnable() {
             @Override
             public void run() {
-                // try(final PreparedStatement stmt = con.prepareStatement("SELECT * FROM co_block WHERE rolled_back = 0 AND time < 1691774000 ORDER BY time DESC")) {
-                try(final PreparedStatement stmt = con.prepareStatement("SELECT * FROM co_block WHERE rolled_back = 0")) {
-                    try(ResultSet result = stmt.executeQuery()){
-                        while (result.next()){
-                            timePrefix = "[" + new SimpleDateFormat("dd MMM yyyy HH:mm:ss").format(new Date(Long.parseLong(result.getInt("time") + "000"))) + "] ";
-                            precent = "[" + Math.round(( ((double) count / 62_000_000) * 100 ) * 1e10) / 1e10 + "%] ";
-
-                            Location loc = new Location(Bukkit.getWorld(RestoreWorld.getInstance().dataBase.getWorld(result.getInt("wid"))), result.getInt("x"), result.getInt("y"), result.getInt("z"));
-                            if (RestoreWorld.getInstance().store.checkExists(loc)) {
-                                System.out.println(timePrefix + precent + "SKIP");
-                                continue;
+                try {
+                    System.out.println("Формируем запрос...");
+                    String query = "SELECT wid, x, y, z, blockdata, time, action, type " +
+                            "FROM co_block " +
+                            "WHERE rolled_back = 0 " +
+                            "ORDER BY wid, x, y, z, time DESC";
+                    final PreparedStatement stmt = con.prepareStatement(query);
+                    System.out.println("Получили данные!");
+                    ResultSet result = stmt.executeQuery();
+                    while (result.next()) {
+                        String meta = "";
+                        if (result.getString("blockdata") != null) {
+                            result.getString("blockdata").split(",");
+                            String[] dataInt = result.getString("blockdata").split(",");
+                            String[] data = new String[dataInt.length];
+                            for (int i = 0; i < dataInt.length; i++) {
+                                data[i] = RestoreWorld.getInstance().dataBase.getBlockData(Integer.parseInt(dataInt[i]));
                             }
-                            RestoreWorld.getInstance().store.addLocation(loc);
-                            //Block block = loc.getBlock();
-                            String meta = "";
-                            if (result.getString("blockdata") != null){
-                                result.getString("blockdata").split(",");
-                                String[] dataInt = result.getString("blockdata").split(",");
-                                String[] data = new String[dataInt.length];
-                                for (int i = 0; i < dataInt.length; i++){
-                                    data[i] =  RestoreWorld.getInstance().dataBase.getBlockData(Integer.parseInt(dataInt[i]));
-                                }
-                                meta = String.join(",",data);
-                            }
-                            RestoreWorld.getInstance().blockDataManager.addLocationData(loc, meta, result.getInt("action"), RestoreWorld.getInstance().dataBase.getMaterial(result.getInt("type")));
-                            // checkTime.put(new BlockData(loc, meta, result.getInt("action"), RestoreWorld.getInstance().dataBase.getMaterial(result.getInt("type"))), (long) result.getInt("time"));
-                            count++;
-
+                            meta = String.join(",", data);
                         }
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
+
+                        // Обработка результатов
+                        Location loc = new Location(Bukkit.getWorld(RestoreWorld.getInstance().dataBase.getWorld(result.getInt("wid"))), result.getInt("x"), result.getInt("y"), result.getInt("z"));
+                        int type = result.getInt("type");
+                        int action = result.getInt("action");
+                        RestoreWorld.getInstance().blockDataManager.addLocationData(loc, meta, action, getMaterial(type));
+                        //RestoreWorld.getInstance().blockDataManager.addLocationData(loc, meta, result.getInt("action"), RestoreWorld.getInstance().dataBase.getMaterial(result.getInt("type")));
+
                     }
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                isFinished = true;
-                System.out.println("Finish");
+                isFinishedQuery = true;
             }
         }.runTaskAsynchronously(RestoreWorld.getInstance());
     }
 
-    public void setBlock() {
-        BlockData blockDataSave = null;
-        try {
-            for (BlockData blockData : RestoreWorld.getInstance().blockDataManager.getLocationDataList()) {
-                blockDataSave = blockData;
+    public void setBlock(){
+        if (!isFinishedQuery) {
+            System.out.println("Ожидание окончание работы бд");
+            return;
+        }
+        if (isStartedSetBlock) {
+            return;
+        }
+        isStartedSetBlock = true;
+        System.out.println("Запуск установки блоков");
+        Iterator<BlockData> iterator = RestoreWorld.getInstance().blockDataManager.getLocationDataList().iterator();
+        while (iterator.hasNext()) {
+            BlockData blockData = iterator.next();
+            try {
+                System.out.println(blockData.getMaterial() + " " + blockData.getAction() + " " + blockData.getLocation().getBlockX() + " " + blockData.getLocation().getBlockY() + " " + blockData.getLocation().getBlockZ());
                 Location locationData = blockData.getLocation();
-                /*if (checkTime.containsKey(blockData) ) {
-
-                }
-                else {
-                    checkTime.put(blockData, )
-                }*/
                 if (blockData.getAction() == 1) {
-                    locationData.getBlock().setBlockData(RestoreWorld.getInstance().getServer().createBlockData(blockData.getMaterial()+"["+blockData.getMeta()+"]"));
-                }
-                else {
+                    locationData.getBlock().setBlockData(RestoreWorld.getInstance().getServer().createBlockData(blockData.getMaterial() + "[" + blockData.getMeta() + "]"));
+                } else {
                     locationData.getBlock().setType(Material.AIR);
                 }
-                RestoreWorld.getInstance().blockDataManager.removeBlockData(blockData.getLocation(), blockData.getMeta(), blockData.getAction(), blockData.getMaterial());
-                System.out.println(timePrefix + precent + blockData.getMaterial() + " | " + blockData.getLocation().getBlockX() + " " + blockData.getLocation().getBlockY() + " " + blockData.getLocation().getBlockZ());
+                iterator.remove();
+            } catch (Exception e) {
+                iterator.remove();
+                RestoreWorld.getInstance().getLogger().warning("EXCEPTION!EXCEPTION!EXCEPTION!");
             }
-        } catch (Exception e) {
-            if (blockDataSave != null) {
-                RestoreWorld.getInstance().getLogger().info("Удаление из массива после ошибки");
-                RestoreWorld.getInstance().blockDataManager.removeBlockData(blockDataSave.getLocation(), blockDataSave.getMeta(), blockDataSave.getAction(), blockDataSave.getMaterial());
-            }
-            RestoreWorld.getInstance().getLogger().warning("EXCEPTION!EXCEPTION!EXCEPTION!");
         }
-        if (isFinished && RestoreWorld.getInstance().blockDataManager.getLocationDataList().size() < 1) async.cancel();
-    }
+        if (RestoreWorld.getInstance().blockDataManager.getLocationDataList().isEmpty()) {
+            async.cancel();
 
-    public void updateBlocks(){
-        try(final PreparedStatement stmt = this.con.prepareStatement("SELECT * FROM co_block WHERE rolled_back = 0 AND time < 1691774000 ORDER BY time DESC")) {
-            try(ResultSet result = stmt.executeQuery()){
-                while (result.next()){
-                    String timePrefix = "[" + new SimpleDateFormat("dd MMM yyyy HH:mm:ss").format(new Date(Long.parseLong(result.getInt("time") + "000"))) + "] ";
-                    String precent = "[" + Math.round(( ((double) count / 62_000_000) * 100 ) * 1e10) / 1e10 + "%] ";
-
-                    Location loc = new Location(Bukkit.getWorld(RestoreWorld.getInstance().dataBase.getWorld(result.getInt("wid"))), result.getInt("x"), result.getInt("y"), result.getInt("z"));
-                    if (RestoreWorld.getInstance().store.checkExists(loc)) {
-                        System.out.println(timePrefix + precent + "SKIP");
-                        continue;
-                    }
-                    RestoreWorld.getInstance().store.addLocation(loc);
-                    Block block = loc.getBlock();
-                    String meta = "";
-                    if (result.getString("blockdata") != null){
-                        result.getString("blockdata").split(",");
-                        String[] dataInt = result.getString("blockdata").split(",");
-                        String[] data = new String[dataInt.length];
-                        for (int i = 0; i < dataInt.length; i++){
-                            data[i] =  RestoreWorld.getInstance().dataBase.getBlockData(Integer.parseInt(dataInt[i]));
-                        }
-                        meta = String.join(",",data);
-                    }
-                    try {
-                        if (result.getString("action").equalsIgnoreCase("1"))
-                            block.setBlockData(RestoreWorld.getInstance().getServer().createBlockData(RestoreWorld.getInstance().dataBase.getMaterial(result.getInt("type"))+"["+meta+"]"));
-                        else if (result.getString("action").equalsIgnoreCase("0")) {
-                            block.setType(Material.AIR);
-                        }
-                        System.out.println(timePrefix + precent + block.getType().toString() + " | " + block.getX() + " " + block.getY() + " " + block.getZ());
-                        count++;
-                    } catch (Exception e) {
-                        System.out.println(timePrefix + precent + "EXCEPTION");
-                        count++;
-                        continue;
-                    }
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
-        System.out.println("Finish");
     }
 
     public String getWorld(int id){
